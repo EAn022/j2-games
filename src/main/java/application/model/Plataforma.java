31ddbe48397379ac748bdec846909553938422cb
EAn022 <162371161+EAn022@users.noreply.github.com>
2025-05-23 16:55:18 +0000

    fix:annotations

--- src/main/java/application/model/Plataforma.java
+++ src/main/java/application/model/Plataforma.java
@@ -5,8 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@Table(name = "plataformas")
 public class Plataforma {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
