3ea13c4d6721f9b0e0a6221bfde26975c922787c
eduardoAN <eduardo.nunes16@fatec.sp.gov.br>
2025-05-25 00:46:52 -0300

    feat:plataformaController insert()

--- src/main/java/application/controller/PlataformaController.java
+++ src/main/java/application/controller/PlataformaController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import application.model.Plataforma;
 import application.repository.PlataformaRepository;
 
 @Controller
@@ -20,5 +23,18 @@ public class PlataformaController {
         return "/plataforma/list";
     }
 
+    @RequestMapping(value = "/insert")
+    public String insert(){
+        return "/plataforma/insert";
+    }
 
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(@RequestParam("descricao") String descricao){
+        Plataforma plataforma = new Plataforma();
+        plataforma.setDescricao(descricao);
+
+        plataformaRepo.save(plataforma);
+
+        return "redirect:/plataforma/insert";
+    }
 }
---
+++ src/main/webapp/WEB-INF/plataforma/insert.jsp
@@ -0,0 +1,22 @@
+<%@ page contentType = "text/html, charset = UTF-8" language = "java"%>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plataforma Insert</title>
+</head>
+<body>
+    <h1>Nova Plataforma</h1>
+
+    <a href="/plataforma/list">Plataforma List</a>
+
+    <form action="/plataforma/insert" method="post">
+        <label for="descricao">Descrição</label>
+        <input type="text" name="descricao">
+
+        <button type="submit">Salvar</button>
+    </form>
+</body>
+</html>
