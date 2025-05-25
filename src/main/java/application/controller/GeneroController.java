fca72097da896a6b04507e244438fcecfaf201b2
eduardoAN <eduardo.nunes16@fatec.sp.gov.br>
2025-05-24 23:17:32 -0300

    feat: generoControle insert()

--- src/main/java/application/controller/GeneroController.java
+++ src/main/java/application/controller/GeneroController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import application.model.Genero;
 import application.repository.GeneroRepository;
 
 @Controller
@@ -20,5 +23,18 @@ public class GeneroController {
         return "/genero/list";
     }
 
-    
+    @RequestMapping(value = "/insert")
+    public String insert(){
+        return "genero/insert";
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(@RequestParam("name") String name){
+        Genero genero = new Genero();
+        genero.setNome(name);
+
+        generoRepo.save(genero);
+        
+        return "redirect:/genero/insert";
+    }
 }
---
+++ src/main/webapp/WEB-INF/genero/insert.jsp
@@ -0,0 +1,22 @@
+<%@ page contentType = "text/html, charset = UTF-8" language = "java"%>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Genero insert</title>
+</head>
+<body>
+    <h1>Novo Genero</h1>
+    
+    <a href="/genero/list">Genero list</a>
+
+    <form action="/genero/insert" method="post">
+        <label for="name">Nome</label>
+        <input type="text" name="name">
+        
+        <button type="submit">Salvar</button>
+    </form>
+</body>
+</html>
